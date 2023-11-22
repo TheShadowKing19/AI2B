eb8a34c08242dbfee95d7820e4faaf23d0049e4a
Mikołaj Jóźwik <mikiwolf19@gmail.com>
2023-11-22 11:37:10 +0100

    Initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HousingLocationComponent} from "../housing-location/housing-location.component";
+import {Housinglocation} from "../housinglocation";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    CommonModule,
+    HousingLocationComponent,
+  ],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocation: Housinglocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+}
